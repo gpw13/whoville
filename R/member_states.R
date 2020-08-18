001098770a23e5e47d07310bbe98ff8d855f4af7
Seth Caldwell <caldwellst@gmail.com>
2020-08-18 09:55:13 +0100

    Add function to get large member states

--- R/member_states.R
+++ R/member_states.R
@@ -17,11 +17,11 @@ is_who_member <- function(iso3) {
   members[idx] %in% TRUE
 }
 
-#' Get membership status from ISO3 codes.
+#' Get small state membership status from ISO3 codes.
 #'
 #' `is_small_member_state()` takes in a vector of ISO3 codes and returns a logical vector
-#' on whether that country is a  small WHO member state or not. In this instance,
-#' small member states are defined as member states with a 2018 total population <=
+#' on whether that country is a small WHO member state or not. In this instance,
+#' small member states are defined as member states with a total population <=
 #' 90,000 in mid-year 2018, according to the World Population Prospects.
 #'
 #' @param iso3 Character vector of ISO3 codes.
@@ -36,4 +36,20 @@ is_small_member_state <- function(iso3) {
   members <- countries[["small_member_state"]]
   idx <- match(iso3, countries[["iso3"]])
   members[idx] %in% TRUE
+}
+
+#' Get large state membership status from ISO3 codes.
+#'
+#' `is_large_member_state()` takes in a vector of ISO3 codes and returns a logical vector
+#' on whether that country is a large WHO member state or not. In this instance,
+#' large member states are defined as member states with a total population >
+#' 90,000 in mid-year 2018, according to the World Population Prospects.
+#'
+#' @param iso3 Character vector of ISO3 codes.
+#'
+#' @return Logical vector.
+#'
+#' @export
+is_large_member_state <- function(iso3) {
+  is_who_member(iso3) & !is_small_member_state(iso3)
 }
