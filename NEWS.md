c8ec177e725cab44d8589e1b3b6e6ec7b22444a5
Seth Caldwell <caldwellst@gmail.com>
2020-09-02 17:12:53 +0100

    Update to 0.1.1, added function to return ISO3 of member states

---
+++ NEWS.md
@@ -0,0 +1,11 @@
+# whotilities 0.1.1
+
+* Added a `NEWS.md` file to track changes to the package.
+* Added `who_member_states()` to quickly extract ISO3 codes for all WHO member states
+
+# whotilities 0.1.0
+
+* Initial release of package
+* Publication of `countries` dataframe with codes, names, and regional categorizations
+* Added functions to coerce names to codes (and vice-versa) released
+* Added functions to get key country information using ISO3 codes
--- R/member_states.R
+++ R/member_states.R
@@ -53,3 +53,32 @@ is_small_member_state <- function(iso3) {
 is_large_member_state <- function(iso3) {
   is_who_member(iso3) & !is_small_member_state(iso3)
 }
+
+#' Get ISO3 codes for WHO member states.
+#'
+#' `who_member_states()` returns ISO3 codes for WHO member states. By default,
+#' it returns all countries, but can be subset to only small (less than 90,000
+#' population) or large states. Useful to expand data frames to explicitly include
+#' missing data for countries.
+#'
+#' @param include Subset of member states to return.
+#'     * "all": All member states (the default)
+#'     * "small": Small member states (2018 population < 90,000)
+#'     * "large": Large member states (2018 population >= 90,000)
+#'
+#' @return A character vector of ISO3 codes.
+#'
+#' @export
+who_member_states <- function(include = c("all", "small", "large")) {
+  include <- rlang::arg_match(include)
+  utils::data("countries",
+              envir = environment(),
+              package = "whotilities")
+  x <- countries[["iso3"]]
+  funs <- c("all" = "is_who_member",
+            "small" = "is_small_member_state",
+            "large" = "is_large_member_state")
+  which_f <- funs[match(include, names(funs))]
+  f <- match.fun(which_f)
+  x[f(x)]
+}
